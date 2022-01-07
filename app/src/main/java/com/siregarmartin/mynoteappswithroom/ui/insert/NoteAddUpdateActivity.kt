de21f3f5445718104020f80c9668cf8e68436aea
Office PC <72garmartin@gmail.com>
2022-01-07 18:12:17 +0700

    connect NoteAddUpdateViewModel

--- app/src/main/java/com/siregarmartin/mynoteappswithroom/ui/insert/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/siregarmartin/mynoteappswithroom/ui/insert/NoteAddUpdateActivity.kt
@@ -2,11 +2,70 @@ package com.siregarmartin.mynoteappswithroom.ui.insert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.siregarmartin.mynoteappswithroom.R
+import com.siregarmartin.mynoteappswithroom.database.Note
+import com.siregarmartin.mynoteappswithroom.databinding.ActivityNoteAddUpdateBinding
+import com.siregarmartin.mynoteappswithroom.helper.ViewModelFactory
 
 class NoteAddUpdateActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
+    private var isEdit = false
+    private var note: Note? = null
+
+    private lateinit var noteAddUpdateViewModel: NoteAddUpdateViewModel
+
+    private var _activityNoteAddUpdateBinding: ActivityNoteAddUpdateBinding? = null
+    private val binding get() = _activityNoteAddUpdateBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_add_update)
+
+        _activityNoteAddUpdateBinding = ActivityNoteAddUpdateBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        noteAddUpdateViewModel = obtainViewModel(this@NoteAddUpdateActivity)
+
+        note = intent.getParcelableExtra(EXTRA_NOTE)
+        if (note != null) {
+            isEdit = true
+        } else {
+            note = Note()
+        }
+
+        val actionBarTitle: String
+        val btnTitle: String
+
+        if (isEdit) {
+            actionBarTitle = getString(R.string.change)
+            btnTitle = getString(R.string.update)
+            if (note != null) {
+                note?.let { note ->
+                    binding?.edtTitle?.setText(note.title)
+                    binding?.edtDescription?.setText(note.description)
+                }
+            }
+        } else {
+            actionBarTitle = getString(R.string.add)
+            btnTitle = getString(R.string.save)
+        }
+
+        supportActionBar?.title = actionBarTitle
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding?.btnSubmit?.text = btnTitle
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityNoteAddUpdateBinding = null
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): NoteAddUpdateViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(NoteAddUpdateViewModel::class.java)
     }
 }
